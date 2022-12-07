06f5ce4fa0bca57a56afd334dbf84f80203bbfef
dz-adman <arun.akdhiman@gmail.com>
2022-12-08 02:23:59 +0530

    First Commit

---
+++ src/main/java/com/dz/ddeq/DdeqApplication.java
@@ -0,0 +1,13 @@
+package com.dz.ddeq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdeqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdeqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dz/ddeq/DdeqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dz.ddeq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdeqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
